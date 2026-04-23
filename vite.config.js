6181f145e448bc32f5c95ce259985229d478f5df
PedroPechKoh <ppechkoh@gmail.com>
2026-04-23 02:15:54 -0600

    Celular

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,13 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
+      
+      // 👇 AQUÍ AGREGAMOS LA REGLA MÁGICA PARA EL PESO 👇
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4000000 // Sube el límite a 4 MB
+      },
+      // 👆 👆 👆
+
       manifest: {
         short_name: "Agente",
         name: "Agente Solutions",
@@ -17,12 +24,12 @@ export default defineConfig({
         theme_color: "#FF6600",
         icons: [
           {
-            src: "/Logo_192.png", // Asegúrate de tener esta imagen en tu carpeta public
+            src: "/Logo_192.png", 
             type: "image/png",
             sizes: "192x192"
           },
           {
-            src: "/Logo_512.png", // Asegúrate de tener esta imagen en tu carpeta public
+            src: "/Logo_512.png", 
             type: "image/png",
             sizes: "512x512"
           }
