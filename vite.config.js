0d990b6518d7cea250722d1b35a0d2c11b4989c4
PedroPechKoh <ppechkoh@gmail.com>
2026-05-14 23:54:42 -0600

    cache

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,17 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'auto',
-      
-      // 👇 AQUÍ AGREGAMOS LA REGLA MÁGICA PARA EL PESO 👇
+
       workbox: {
-        maximumFileSizeToCacheInBytes: 4000000 // Sube el límite a 4 MB
+        // Tu regla para el peso máximo de archivos
+        maximumFileSizeToCacheInBytes: 4000000,
+
+        // 👇 NUEVAS REGLAS PARA FORZAR ACTUALIZACIÓN 👇
+        cleanupOutdatedCaches: true, // Borra la caché de versiones viejas en el celular
+        skipWaiting: true,           // Obliga al nuevo Service Worker a activarse de inmediato
+        clientsClaim: true           // Obliga a las pestañas abiertas a usar la versión nueva
+        // 👆 👆 👆
       },
-      // 👆 👆 👆
 
       manifest: {
         short_name: "Agente",
@@ -24,12 +29,12 @@ export default defineConfig({
         theme_color: "#FF6600",
         icons: [
           {
-            src: "/Logo_192.png", 
+            src: "/Logo_192.png",
             type: "image/png",
             sizes: "192x192"
           },
           {
-            src: "/Logo_512.png", 
+            src: "/Logo_512.png",
             type: "image/png",
             sizes: "512x512"
           }
