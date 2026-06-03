66444ce9d532a0f1560f7a864b5375a284df3a77
PedroPechKoh <ppechkoh@gmail.com>
2026-06-02 23:46:40 -0600

    -boton

--- src/components/VistaCliente/MainLayoutCliente.jsx
+++ src/components/VistaCliente/MainLayoutCliente.jsx
@@ -67,7 +67,6 @@ const MainLayoutCliente = ({ children }) => {
 
   const propertyNavButtons = [
     { label: 'DETALLES PROPIEDAD', path: tableroPath, icon: <Home size={18} /> },
-    { label: 'VER LEVANTAMIENTO', path: detailPath, icon: <FileText size={18} /> },
     { label: 'SOS', path: '/SOSView', icon: <Bell size={18} /> },
     { label: 'COTIZACIONES', path: '/vista-cotizaciones', icon: <FileText size={18} /> },
   ];
