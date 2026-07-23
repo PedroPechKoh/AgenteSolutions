eaa51224b55eaadbdf5a40ce4a97886eb0106622
Alejandra Alcocer <viviburgos13@gmail.com>
2026-07-22 20:43:56 -0600

    ..

--- src/components/ClientRegister.jsx
+++ src/components/ClientRegister.jsx
@@ -255,7 +255,7 @@ const ClientRegister = () => {
         .back-button {
           position: fixed;
           top: 50px;
-          left: 25px;
+          left: 50px;
           background: rgba(255, 255, 255, 0.05);
           border: 1px solid rgba(255, 255, 255, 0.2);
           color: white;
