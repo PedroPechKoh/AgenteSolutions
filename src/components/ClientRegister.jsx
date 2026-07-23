fa4f6989309d832e3df68283e2a105b26fdf6709
Alejandra Alcocer <viviburgos13@gmail.com>
2026-07-22 20:43:15 -0600

    retroceder

--- src/components/ClientRegister.jsx
+++ src/components/ClientRegister.jsx
@@ -254,7 +254,7 @@ const ClientRegister = () => {
       <style>{`
         .back-button {
           position: fixed;
-          top: 25px;
+          top: 50px;
           left: 25px;
           background: rgba(255, 255, 255, 0.05);
           border: 1px solid rgba(255, 255, 255, 0.2);
